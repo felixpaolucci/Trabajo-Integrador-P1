8370d27dff58b9e8e6f27573ec46e32b2a2fbda9
Bautista Lanussegit <blanusse@udesa.edu.ar>
2020-11-20 15:34:17 -0300

    progreso

--- js/master.js
+++ js/master.js
@@ -1,13 +1,14 @@
 window.addEventListener('load', function(){
     // SLIDER:
-    let slider = document.querySelector('.slider')
+    let sliderjs = document.querySelector('.sliderjs')
+    sliderjs.classList.add('slider')
     fetch('https://api.themoviedb.org/3/movie/popular?api_key=947976bd814222f623ebca2e4e5e8a3a&language=es-ES&page=1')
     .then(respuesta =>{
         return respuesta.json()
     })
     .then(function(sliderA){
         for(let i = 4; i < 8; i ++){
-            slider.innerHTML += `<li> <img src="https://image.tmdb.org/t/p/original${sliderA.results[i].backdrop_path}" alt="slider" </li>`
+            sliderjs.innerHTML += `<li class='slider'> <img src="https://image.tmdb.org/t/p/original${sliderA.results[i].backdrop_path}" alt="slider" </li>`
         }
     })
 
@@ -52,7 +53,49 @@ window.addEventListener('load', function(){
     .catch(function(error){
         console.log(error);
     })
+    
 
+     // NO TE PODES PERDER:
+     let peliculasNo = document.querySelector('.peliculasNo')
+
+     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=bad307a59294abaae8c2d0fcc48475d8&language=es-ES&page=1')
+     .then(function(respuesta){
+         return respuesta.json()
+     })
+     .then(function(no){
+         console.log(no);
+         no.results.forEach(pelicula => {
+             let articulo = document.createElement('article')
+             articulo.innerHTML += `<a href="movieDetail.html?id=${pelicula.id}&titulo=${pelicula.title}&imagen=${pelicula.poster_path}&overview=${pelicula.overview}&calificacion=${pelicula.vote_average}&genero=${pelicula.genre_ids}&fechapubli=${pelicula.release_date}"> <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt='${pelicula.title}'/> </a> <p>${pelicula.title}</p> <br>`
+ 
+             peliculasNo.append(articulo)
+         })
+     })
+     .catch(function(error){
+         console.log(error);
+     })
+        // SERIES:
+        let series = document.querySelector('.series')
+
+        fetch('https://api.themoviedb.org/3/movie/latest?api_key=bad307a59294abaae8c2d0fcc48475d8&language=es-ES')
+        .then(function(respuesta){
+            return respuesta.json()
+        })
+        .then(function(series1){
+            console.log(series1);
+            series.results.forEach(pelicula => {
+                let articulo = document.createElement('article')
+                articulo.innerHTML += `<a href="movieDetail.html?id=${pelicula.id}&titulo=${pelicula.title}&imagen=${pelicula.poster_path}&overview=${pelicula.overview}&calificacion=${pelicula.vote_average}&genero=${pelicula.genre_ids}&fechapubli=${pelicula.release_date}"> <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt='${pelicula.title}'/> </a> <p>${pelicula.title}</p> <br>`
+    
+                series.append(articulo)
+            })
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+        
+     
+ 
     
 
 })
