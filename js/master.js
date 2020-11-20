8855497621f1549c8f8c4fa431be5215b7dbb9ff
felixpaolucci <fpaolucci@udesa.edu.ar>
2020-11-20 16:13:27 -0300

    + cambios

--- js/master.js
+++ js/master.js
@@ -56,46 +56,42 @@ window.addEventListener('load', function(){
     
 
      // NO TE PODES PERDER:
-     let peliculasNo = document.querySelector('.peliculasNo')
+    let peliculasNo = document.querySelector('.peliculasNo')
 
-     fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=bad307a59294abaae8c2d0fcc48475d8&language=es-ES&page=1')
-     .then(function(respuesta){
-         return respuesta.json()
-     })
-     .then(function(no){
-         console.log(no);
-         no.results.forEach(pelicula => {
-             let articulo = document.createElement('article')
-             articulo.innerHTML += `<a href="movieDetail.html?id=${pelicula.id}&titulo=${pelicula.title}&imagen=${pelicula.poster_path}&overview=${pelicula.overview}&calificacion=${pelicula.vote_average}&genero=${pelicula.genre_ids}&fechapubli=${pelicula.release_date}"> <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt='${pelicula.title}'/> </a> <p>${pelicula.title}</p> <br>`
- 
-             peliculasNo.append(articulo)
-         })
-     })
-     .catch(function(error){
-         console.log(error);
-     })
-        // SERIES:
-        let series = document.querySelector('.series')
+    fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=bad307a59294abaae8c2d0fcc48475d8&language=es-ES&page=1')
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then(function(no){
+        console.log(no);
+        no.results.forEach(pelicula => {
+            let articulo = document.createElement('article')
+            articulo.innerHTML += `<a href="movieDetail.html?id=${pelicula.id}&titulo=${pelicula.title}&imagen=${pelicula.poster_path}&overview=${pelicula.overview}&calificacion=${pelicula.vote_average}&genero=${pelicula.genre_ids}&fechapubli=${pelicula.release_date}"> <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt='${pelicula.title}'/> </a> <p>${pelicula.title}</p> <br>`
 
-        fetch('https://api.themoviedb.org/3/movie/latest?api_key=bad307a59294abaae8c2d0fcc48475d8&language=es-ES')
-        .then(function(respuesta){
-            return respuesta.json()
+            peliculasNo.append(articulo)
         })
-        .then(function(series1){
-            console.log(series1);
-            series.results.forEach(pelicula => {
-                let articulo = document.createElement('article')
-                articulo.innerHTML += `<a href="movieDetail.html?id=${pelicula.id}&titulo=${pelicula.title}&imagen=${pelicula.poster_path}&overview=${pelicula.overview}&calificacion=${pelicula.vote_average}&genero=${pelicula.genre_ids}&fechapubli=${pelicula.release_date}"> <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt='${pelicula.title}'/> </a> <p>${pelicula.title}</p> <br>`
-    
-                series.append(articulo)
-            })
-        })
-        .catch(function(error){
-            console.log(error);
-        })
-        
-     
- 
-    
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 
+     // SERIES:
+    let peliculaSeries = document.querySelector('.peliculaSeries')
+
+    fetch('https://api.themoviedb.org/3/tv/popular?api_key=947976bd814222f623ebca2e4e5e8a3a&language=es-ES&page=1')
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then(function(cualquiera){
+        console.log(cualquiera);
+        cualquiera.results.forEach(pelicula => {
+            let articulo = document.createElement('article')
+            articulo.innerHTML += `<a href="movieDetail.html?id=${pelicula.id}&titulo=${pelicula.title}&imagen=${pelicula.poster_path}&overview=${pelicula.overview}&calificacion=${pelicula.vote_average}&genero=${pelicula.genre_ids}&fechapubli=${pelicula.release_date}"> <img src='https://image.tmdb.org/t/p/w500${pelicula.poster_path}' alt='${pelicula.title}'/> </a> <p>${pelicula.original_name}</p> <br>`
+
+            peliculaSeries.append(articulo)
+        })
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 })
